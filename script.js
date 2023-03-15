402a248c31e99fc500762854e91cd42914a0c3df
atharvaawatade <97244560+atharvaawatade@users.noreply.github.com>
2023-03-15 18:17:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+var typed = new Typed('.multiple-text', {
+    strings: ['Atharva Awatade', 'Developer', 'Designer'],
+    typeSpeed: 100,
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
